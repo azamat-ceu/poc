6fa15a6c2652122d3cb96a897d34149c3a24a433
1 <1@303-14>
2020-03-06 16:14:49 +0500

    прост

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+
+namespace WindowsFormsApp3
+{
+    
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=303-14;Initial Catalog=azam;Integrated security=true;");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand com = new SqlCommand($"Select [FIO_manager] From manager where Login_manager = '{textBox1.Text}' and Password = '{textBox2.Text}'", con);
+            SqlDataReader dr = com.ExecuteReader();
+            if (dr.HasRows)
+            {
+                dr.Read();
+                MessageBox.Show("Вход успешно выполнен");
+                Form2 f = new Form2();
+                f.Show();
+                this.Hide();
+                dr.Close();
+            }
+            else
+                MessageBox.Show("Отойди");
+            con.Close();
+            
+
+
+    }
+
+    private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form2 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=303-14;Initial Catalog=azam;Integrated security=true;");
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+        public int id;
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand com = new SqlCommand($"Select * From coeefs, grade where coeefs.id = '{id}' and coeefs.id = grade.id", con);
+            SqlDataReader dr = com.ExecuteReader();
+            if (dr.HasRows)
+            {
+                dr.Read();
+                textBox1.Text = dr["Junior"].ToString();
+                textBox2.Text = dr["Middle"].ToString();
+                textBox3.Text = dr["Senior"].ToString();
+                textBox4.Text = dr["coef_analiz"].ToString();
+                textBox5.Text = dr["coef_device"].ToString();
+                textBox6.Text = dr["coef_service"].ToString();
+                textBox7.Text = dr["coef_time"].ToString();
+                textBox8.Text = dr["coef_complexity"].ToString();
+                textBox9.Text = dr["coef_money"].ToString();
+                dr.Close();
+
+            }
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
