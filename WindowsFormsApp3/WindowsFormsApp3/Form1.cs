9aad7166e924d797c4cdf7fabb0379790f355c30
1 <1@303-14>
2020-03-06 16:31:07 +0500

    Добавил коменты

--- WindowsFormsApp3/WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp3
     
     public partial class Form1 : Form
     {
-        SqlConnection con = new SqlConnection("Data Source=303-14;Initial Catalog=azam;Integrated security=true;");
+        SqlConnection con = new SqlConnection("Data Source=303-14;Initial Catalog=azam;Integrated security=true;");//подключение БД
         public Form1()
         {
             InitializeComponent();
@@ -25,19 +25,19 @@ namespace WindowsFormsApp3
         private void button1_Click(object sender, EventArgs e)
         {
             con.Open();
-            SqlCommand com = new SqlCommand($"Select [FIO_manager] From manager where Login_manager = '{textBox1.Text}' and Password = '{textBox2.Text}'", con);
+            SqlCommand com = new SqlCommand($"Select [FIO_manager] From manager where Login_manager = '{textBox1.Text}' and Password = '{textBox2.Text}'", con);//создания запроса на проверку логина и пароля
             SqlDataReader dr = com.ExecuteReader();
-            if (dr.HasRows)
+            if (dr.HasRows)//Проверка пароля и логина 
             {
                 dr.Read();
                 MessageBox.Show("Вход успешно выполнен");
                 Form2 f = new Form2();
-                f.Show();
-                this.Hide();
-                dr.Close();
+                f.Show();//открытие второй формы
+                this.Hide();//скрытие первой формы
+                dr.Close();//закрытие
             }
             else
-                MessageBox.Show("Отойди");
+                MessageBox.Show("Отойди");//если не правильно авторизовался выходит сообщение "отойди"
             con.Close();
             
 
